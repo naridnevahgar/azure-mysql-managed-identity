2333f79749b01870577319de0d3f7882c2062ca9
naridnevahgar <naridnevahgar@gmail.com>
2021-03-24 18:05:50 +0900

    cleanup

--- src/main/java/com/rs/AzureMysqlManagedIdentityDemo/AzureMysqlManagedIdentityDemoApplication.java
+++ src/main/java/com/rs/AzureMysqlManagedIdentityDemo/AzureMysqlManagedIdentityDemoApplication.java
@@ -1,15 +1,8 @@
 package com.rs.AzureMysqlManagedIdentityDemo;
 
-import com.azure.core.credential.TokenRequestContext;
-import com.azure.identity.ManagedIdentityCredential;
-import com.azure.identity.ManagedIdentityCredentialBuilder;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
